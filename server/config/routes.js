589f2d30c136171f32ecaaddfc0ac5e5d2df59d8
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2017-12-16 23:02:05 +0200

    ordered the routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -10,27 +10,21 @@ module.exports = (app) => {
   app.get('/about', auth.isAuthenticated, controllers.home.about);
 
   app.get('/users/register', auth.isAuthenticated, controllers.users.registerGet);
-  app.post('/users/register', controllers.users.registerPost);
-
   app.get('/users/login', controllers.users.loginGet);
+  app.post('/users/register', controllers.users.registerPost);
   app.post('/users/login', controllers.users.loginPost);
-
   app.post('/users/logout', controllers.users.logout);
 
   app.get('/cameras', auth.isAuthenticated, controllers.camera.getStream);
+  app.post('/cameras/add', auth.isAuthenticated, controllers.camera.addCamera);
+  app.post('/cameras/remove', auth.isAuthenticated, controllers.camera.removeCamera);
 
-  app.get('/cameras', controllers.camera.getStream);
-  app.post('/cameras/add', controllers.camera.addCamera);
-  app.post('/cameras/remove', controllers.camera.removeCamera);
   app.get('/faces', auth.isAuthenticated, controllers.faces.getFacesPage);
-
-  app.get('/alerts', auth.isAuthenticated, controllers.alerts.getAlertsPage);
-
-  app.post('/faces/add', upload.single('image'), controllers.faces.addFace);
-
   app.get('/faces/remove/:id', controllers.faces.removeFace);
   app.get('/faces/all', controllers.faces.getAllFaces);
+  app.post('/faces/add', upload.single('image'), controllers.faces.addFace);
 
+  app.get('/alerts', auth.isAuthenticated, controllers.alerts.getAlertsPage);
   app.post('/alerts/add', controllers.alerts.addAlert);
 
   app.all('*', (req, res) => {
