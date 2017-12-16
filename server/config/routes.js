8c371564f0c37826148c3253b25ee9e969e9f0f2
DanielSlavov <daxazzbg@gmail.com>
2017-12-16 16:38:53 +0200

    added alert listing from db

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,28 +1,30 @@
-const controllers = require('../controllers');
-const auth = require('./auth');
+const controllers = require("../controllers");
+const auth = require("./auth");
 
-module.exports = (app) => {
-  app.get('/', controllers.home.index);
-  app.get('/about', auth.isAuthenticated, controllers.home.about);
+module.exports = app => {
+  app.get("/", controllers.home.index);
+  app.get("/about", auth.isAuthenticated, controllers.home.about);
 
-  app.get('/users/register', controllers.users.registerGet);
-  app.post('/users/register', controllers.users.registerPost);
+  app.get("/users/register", controllers.users.registerGet);
+  app.post("/users/register", controllers.users.registerPost);
 
-  app.get('/users/login', controllers.users.loginGet);
-  app.post('/users/login', controllers.users.loginPost);
+  app.get("/users/login", controllers.users.loginGet);
+  app.post("/users/login", controllers.users.loginPost);
 
-  app.post('/users/logout', controllers.users.logout);
+  app.post("/users/logout", controllers.users.logout);
 
-  app.get('/cameras', controllers.camera.getStream);
+  app.get("/cameras", controllers.camera.getStream);
 
-  app.get('/faces', controllers.faces.getFacesPage);
+  app.get("/faces", controllers.faces.getFacesPage);
 
-  app.get('/alerts', controllers.alerts.getAlertsPage);
+  app.get("/alerts", controllers.alerts.getAlertsPage);
 
-  app.post('/alerts/add',controllers.alerts.addAlert)
-  app.all('*', (req, res) => {
+  app.post("/alerts/add", controllers.alerts.addAlert);
+
+  
+  app.all("*", (req, res) => {
     res.status(404);
-    res.send('404 Not Found!');
+    res.send("404 Not Found!");
     res.end();
   });
 };
--- server/controllers/alerts-controller.js
+++ server/controllers/alerts-controller.js
@@ -2,10 +2,19 @@ const Alert = require("mongoose").model("Alert");
 
 module.exports = {
   getAlertsPage: (req, res) => {
-    res.render("alerts/alerts");
+    Alert.find((err, alerts) => {
+      console.log(alerts)
+      res.render("alerts/alerts", {alerts:alerts});
+    });
   },
   addAlert: (req, res) => {
     let body = req.body;
-    Alert.create(body).then(alert => {});
+    console.log(req.body);
+
+    Alert.create({
+      name: body.name,
+      image: body.image,
+      timestamp: body.timestamp
+    }).then(alert => {});
   }
 };
