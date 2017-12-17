b20516c53b1ac4f186fa4f2572e645648170d461
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2017-12-17 14:57:12 +0200

    added some missing authentication middleware

--- server/config/routes.js
+++ server/config/routes.js
@@ -11,9 +11,9 @@ module.exports = (app) => {
 
   app.get('/users/register', auth.isAuthenticated, controllers.users.registerGet);
   app.get('/users/login', controllers.users.loginGet);
-  app.post('/users/register', controllers.users.registerPost);
+  app.post('/users/register', auth.isAuthenticated, controllers.users.registerPost);
   app.post('/users/login', controllers.users.loginPost);
-  app.post('/users/logout', controllers.users.logout);
+  app.post('/users/logout', auth.isAuthenticated, controllers.users.logout);
 
   app.get('/cameras', auth.isAuthenticated, controllers.camera.getCameras);
   app.get('/cameras/:id', auth.isAuthenticated, controllers.camera.getStream);
@@ -21,13 +21,13 @@ module.exports = (app) => {
   app.get('/cameras/remove/:id', auth.isAuthenticated, controllers.camera.removeCamera);
 
   app.get('/faces', auth.isAuthenticated, controllers.faces.getFacesPage);
-  app.get('/faces/remove/:id', controllers.faces.removeFace);
+  app.get('/faces/remove/:id', auth.isAuthenticated, controllers.faces.removeFace);
   app.get('/faces/all', controllers.faces.getAllFaces);
-  app.post('/faces/add', upload.single('image'), controllers.faces.addFace);
+  app.post('/faces/add', upload.single('image'), auth.isAuthenticated, controllers.faces.addFace);
 
   app.get('/alerts', auth.isAuthenticated, controllers.alerts.getAlertsPage);
   app.post('/alerts/add', controllers.alerts.addAlert);
-  app.post('/alerts/remove', controllers.alerts.removeAlert);
+  app.post('/alerts/remove', auth.isAuthenticated, controllers.alerts.removeAlert);
 
   app.all('*', (req, res) => {
     res.status(404);
