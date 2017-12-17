0fe50007322e362e3c49e28a1411b5f852fa763c
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2017-12-17 14:32:24 +0200

    removed console logs and fixed some eslint errors

--- server/controllers/alerts-controller.js
+++ server/controllers/alerts-controller.js
@@ -11,9 +11,13 @@ module.exports = {
       names: body.names,
       image: body.image,
       timestamp: body.timestamp,
-    }).then(() => {
-      res.json({ success: 'true', message: 'Alert created!' });
-    });
+    })
+      .then(() => {
+        res.json({ success: 'true', message: 'Alert created!' });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
   removeAlert: (req, res) => {
     const { id } = req.body;
