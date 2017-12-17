0acbb948ab759c1f1240855dc024a11b62efe2b4
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2017-12-17 02:37:03 +0200

    alerts now visualise images

--- server/config/express.js
+++ server/config/express.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
   app.set('view engine', 'pug');
   app.use(cookieParser());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+  app.use(bodyParser.json({ limit: '50mb' }));
   app.use(session({
     secret: 'neshto-taino!@#$%',
     resave: false,
--- server/controllers/alerts-controller.js
+++ server/controllers/alerts-controller.js
@@ -1,20 +1,20 @@
-const Alert = require("mongoose").model("Alert");
+const Alert = require('mongoose').model('Alert');
 
 module.exports = {
   getAlertsPage: (req, res) => {
-    console.log(req.user)
-    Alert.find((err, alerts) => {
-      res.render("alerts/alerts", {alerts:alerts});
-    });
+    console.log(req.user);
+    Alert.find((err, alerts) => res.render('alerts/alerts', { alerts }));
   },
   addAlert: (req, res) => {
-    let body = req.body;
+    const { body } = req;
     console.log(req.body);
 
     Alert.create({
       name: body.name,
       image: body.image,
-      timestamp: body.timestamp
-    }).then(alert => {});
-  }
+      timestamp: body.timestamp,
+    }).then(() => {
+      res.json({ success: 'true', message: 'Alert created!' });
+    });
+  },
 };
